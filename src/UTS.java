7eac0204ec401288663731e9cac224bab8d7d4c9
Wildan77 <boyblanco77@gmail.com>
2023-11-13 13:01:54 +0700

    ini

--- src/UTS.java
+++ src/UTS.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class UTS {
+public class UTS
+public class CodesCracker
+{
+    public static void main(String[] args)
+    {
+        int[] arr = new int[10];
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter 10 Numbers for the Array: ");
+        for(int i=0; i<10; i++)
+            arr[i] = s.nextInt();
+
+        int countEven=0, countOdd=0;
+        for(int i=0; i<10; i++)
+        {
+            if(arr[i]%2==0)
+                countEven++;
+            else
+                countOdd++;
+        }
+
+        System.out.println("\nEven Number: " +countEven);
+        System.out.println("Odd Number: " +countOdd);
+    }
+}
 }
--- src/coba.java
+++ src/coba.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class coba {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class coba {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Input Tiga Angka Dengan Spasi =  ");
+        String[] p_ = reader.readLine().split( "");
+        int _c = Integer.parseInt(p_[0]);
+        int _n = Integer.parseInt(p_[1]);
+        int _z = Integer.parseInt(p_[2]);
+        boolean _bool;
+        if (_c + _n + _z > 100) {
+            _bool = false;
+            if (_bool = true){
+                System.out.println("You are not authorized...");
+        } else if (_bool = false) {
+            int h_ = 0;
+            for (int index = 0; index < 5; index++) {
+                h_ = _c + _n - _z + h_;
+
+            }
+            System.out.println("Total Number is = " + h_);
+        }
+    }else{
+        System.out.println("Conctact yor administartor.....");
+    }
+   }
 }
